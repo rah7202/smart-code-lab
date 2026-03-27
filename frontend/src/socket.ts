20467203e76542f1d1a24d3cacaad34888c682f5
rah7202 <rpidiyar249@gmail.com>
2026-03-27 19:18:10 +0530

    fix : socket connection improvement

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 const URL = import.meta.env.VITE_BACKEND_URL;
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ["websocket"],
+    reconnectionAttempts: 5,
+    reconnection: true,
+}); 
