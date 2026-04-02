d641a2ab7ebaaae0f54004231172df47cc6e1754
rah7202 <rpidiyar249@gmail.com>
2026-04-02 23:06:16 +0530

    fix: wraped required routes in ProtectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,53 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { isTokenValid } from "./utils/auth";
 import Home from "./components/Home";
 import EditorPage from "./components/EditorPage";
-import { Toaster } from "react-hot-toast";
+import Login from "./components/LoginPage";
+import Signup from "./components/Signup";
+import { ProtectedRoute } from "./utils/ProtectedRoute";
 import './App.css'
 
 function App() {
-
-
 
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <Toaster position="top-center" />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/editor/:roomId" element={<EditorPage />} />
+        <Route 
+            path="/login" 
+            element={isTokenValid(localStorage.getItem("token")) ? <Navigate to="/" /> : <Login />}  
+        />
+        
+        <Route 
+            path="/signup" 
+            element={isTokenValid(localStorage.getItem("token")) ? <Navigate to="/" /> : <Signup />}  
+        />
+
+        <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } 
+        />
+
+        <Route 
+            path="/editor/:roomId" 
+            element={
+              <ProtectedRoute>
+                <EditorPage />
+              </ProtectedRoute>
+
+            } 
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+
+
+
       </Routes>
     </div>
   )
