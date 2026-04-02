edeecd8f67910ee0e26a15c54a21ee365a46e071
rah7202 <rpidiyar249@gmail.com>
2026-04-02 22:33:09 +0530

    fix : Added authentication in ai routes

--- backend/src/routes/ai.route.ts
+++ backend/src/routes/ai.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { clearAIHistory, generateResponse, getAIHistory } from "../controllers/ai.controller";
 import { validate, aiGenerateSchema } from "../middleware/validate";
+import { authenticate } from "../middleware/auth.middleware"
 
 const router = Router();
 
-router.post("/generate", validate(aiGenerateSchema), generateResponse);
-router.get("/history/:roomId", getAIHistory);
-router.delete("/history/:roomId", clearAIHistory);
+router.post("/generate", authenticate, validate(aiGenerateSchema), generateResponse);
+router.get("/history/:roomId", authenticate, getAIHistory);
+router.delete("/history/:roomId", authenticate, clearAIHistory);
 
 export default router; 
