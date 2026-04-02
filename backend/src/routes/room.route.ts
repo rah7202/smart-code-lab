594541a111dbbc2332dac57439104d1fa306288f
rah7202 <rpidiyar249@gmail.com>
2026-04-02 22:36:24 +0530

    fix : Added autthentication in routes

--- backend/src/routes/room.route.ts
+++ backend/src/routes/room.route.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { getRoomData, saveRoomCode } from "../controllers/room.controller";
+import { getRoomData, createRoom, saveRoomCode } from "../controllers/room.controller";
 import { validate, roomSaveSchema } from "../middleware/validate";
+import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
 
 // Load room
-router.get("/:roomId", getRoomData);
+router.get("/:roomId", authenticate, getRoomData);
 
 // Save current code (debounced)
-router.post("/:roomId/save",validate(roomSaveSchema), saveRoomCode);
+router.post("/create", authenticate, createRoom );
+router.post("/:roomId/save", authenticate, validate(roomSaveSchema), saveRoomCode);
+
 
 export default router;
