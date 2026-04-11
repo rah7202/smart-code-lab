e7e5dec84706df1be4c51c650de2fa043762755a
rah7202 <rpidiyar249@gmail.com>
2026-04-11 22:25:09 +0530

    fix: generate ai API removed

--- backend/src/services/ai.service.ts
+++ backend/src/services/ai.service.ts
@@ -5,22 +5,12 @@ import { logger } from "../utils/logger";
 const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAi.getGenerativeModel({ model: "gemini-2.5-flash" });
 
-// ----------------GENERATIVE AI RESPONSE--------------------------
-export const generativeAIResponse = async (prompt: string): Promise<string> => {
-    if (!prompt?.trim()) throw new Error("Prompt cannot be empty");
-
-    logger.info(`Generating response (prompt length: ${prompt.length})`);
-
-    const result = await model.generateContent(prompt);
-    const text = result.response.text();
-
-    logger.info(`[AI] Response generated (response length: ${text.length})`);
-    return text;
-};
-
 export async function* streamAIResponse(prompt: string): AsyncGenerator<string> {
+    
     if (!prompt?.trim()) throw new Error("Prompt cannot be empty");
+    
     logger.info(`[AI] Streaming (prompt length: ${prompt.length})`);
+    
     const result = await model.generateContentStream(prompt);
     
     for await (const chunk of result.stream) {
@@ -28,5 +18,4 @@ export async function* streamAIResponse(prompt: string): AsyncGenerator<string> 
         if (text) yield text;
         
     }
-
 }
