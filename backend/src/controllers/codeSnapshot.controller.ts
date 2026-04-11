5a8e77d3f3e4a2cdb7d4cda97080cdaac7c169d7
rah7202 <rpidiyar249@gmail.com>
2026-04-11 22:29:13 +0530

    fix: duplication code snapshot handled + code saved to GCS

--- backend/src/controllers/codeSnapshot.controller.ts
+++ backend/src/controllers/codeSnapshot.controller.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express";
 import { prisma } from "../db/prisma";
 import { saveCodeSnapshot } from "../services/codeSnapshot.service";
+import { getCodeFromGCS, uploadCodeToGCS } from "../lib/storage";
+import { logger } from "../utils/logger";
 
 export const saveSnapshotController = async (req: Request, res: Response) => {
     
     try {
         const { roomId } = req.params;
         const { code, language } = req.body;
+        const codeUrl = await uploadCodeToGCS(code, roomId as string);
         
         await saveCodeSnapshot(
             roomId as string,
-            code as string,
-            language as string
+            codeUrl as string,
+            language as string,
+            code as string,   
         )
 
         res.json({ success: true });
@@ -29,7 +33,19 @@ export const getSnapshotsController = async (req: Request, res: Response) => {
             orderBy: { createdAt: "desc" },
         });
 
-        res.json(snapshots);
+        const result = await Promise.all(
+            
+            snapshots.map(async (snap) => {
+                try {                
+                    const code = await getCodeFromGCS(snap.code);
+                    return { ...snap, code}
+                } catch (err) {
+                    logger.error("Failed to fetch snapshot from GCS : ");
+                    return { ...snap, code: "// Failed to load code" };
+                }
+            })
+        );
+        res.json(result);
     } catch {
         res.status(500).json({ error: "Failed to fetch snapshots"});
     }
